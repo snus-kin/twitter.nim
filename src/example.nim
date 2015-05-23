18bb5b2d1fd1c132763ecdb4daa664ca4dbe73a1
kubo39 <kubo39@gmail.com>
2015-05-24 01:19:36 +0900

    Update example

--- src/example.nim
+++ src/example.nim
@@ -6,5 +6,12 @@ when isMainModule:
                                  "Your Access Token",
                                  "Your Access Token Secret")
 
+  # Simply get.
   var resp = twitterAPI.get("account/verify_credentials.json")
+  echo resp.status
+
+  # Using `callAPI` template.
+  var testStatus: Table[string, string] = initTable[string, string]()
+  testStatus["status"] = "test"
+  resp = twitterAPI.callAPI(statusesUpdate, testStatus)
   echo resp.status
