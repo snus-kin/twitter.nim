08ed2e05166ef98269d6344b4099b0e8d74e5434
kubo39 <kubo39@gmail.com>
2015-05-31 20:19:55 +0900

    Refactoring

--- src/twitter.nim
+++ src/twitter.nim
@@ -38,7 +38,7 @@ proc newTwitterAPI*(consumerToken: ConsumerToken, accessToken, accessTokenSecret
 
 
 proc newTwitterAPI*(consumerKey, consumerSecret, accessToken, accessTokenSecret: string): TwitterAPI =
-  var consumerToken: ConsumerToken = ConsumerToken(consumerKey: consumerKey,
+  let consumerToken: ConsumerToken = ConsumerToken(consumerKey: consumerKey,
                                                    consumerSecret: consumerSecret)
   return TwitterAPI(consumerToken: consumerToken,
                     accessToken: accessToken,
@@ -90,9 +90,9 @@ proc signature*(consumerSecret, accessTokenSecret, httpMethod, url: string, para
 
   keys.sort(cmpIgnoreCase)
 
-  var query: string = keys.mapIt(string, $(it & "=" & params[it])).join("&")
-  var key: string = encodeUrl(consumerSecret) & "&" & encodeUrl(accessTokenSecret)
-  var base: string = httpMethod & "&" & encodeUrl(url) & "&" & encodeUrl(query)
+  let query: string = keys.mapIt(string, $(it & "=" & params[it])).join("&")
+  let key: string = encodeUrl(consumerSecret) & "&" & encodeUrl(accessTokenSecret)
+  let base: string = httpMethod & "&" & encodeUrl(url) & "&" & encodeUrl(query)
 
   return encodeUrl(hmacSha1(key, base).toBase64)
 
@@ -115,7 +115,7 @@ proc buildParams(consumerKey, accessToken: string,
 
 proc request*(twitter: TwitterAPI, endPoint, httpMethod: string,
               additionalParams: Table[string, string] = initTable[string, string]()): Response =
-  var url = baseUrl & endPoint
+  let url = baseUrl & endPoint
   var keys: seq[string] = @[]
 
   var params: Table[string, string] = buildParams(twitter.consumerToken.consumerKey,
@@ -128,9 +128,9 @@ proc request*(twitter: TwitterAPI, endPoint, httpMethod: string,
   for key in params.keys:
     keys.add(key)
 
-  var authorizeKeys = keys.filter(proc(x: string): bool = x.startsWith("oauth_"))
-  var authorize = "OAuth " & authorizeKeys.mapIt(string, $(it & "=" & params[it])).join(",")
-  var path = keys.mapIt(string, $(it & "=" & params[it])).join("&")
+  let authorizeKeys = keys.filter(proc(x: string): bool = x.startsWith("oauth_"))
+  let authorize = "OAuth " & authorizeKeys.mapIt(string, $(it & "=" & params[it])).join(",")
+  let path = keys.mapIt(string, $(it & "=" & params[it])).join("&")
 
   if httpMethod == "GET":
     return httpclient.get(url & "?" & path, "Authorization: " & authorize & "\c\L")
@@ -177,3 +177,23 @@ template callAPI*(twitter: TwitterAPI,
                   api: expr,
                   additionalParams: Table[string, string] = initTable[string, string]()): expr =
   api(twitter, additionalParams)
+
+
+
+when isMainModule:
+
+  import unittest
+
+  suite "test for encodeUrl":
+    # https://dev.twitter.com/oauth/overview/percent-encoding-parameters
+    test "examples from twitter's percent-encoding parameters.":
+      check(encodeUrl("Ladies + Gentlemen") == "Ladies%20%2B%20Gentlemen")
+      check(encodeUrl("An encoded string!") == "An%20encoded%20string%21")
+      check(encodeUrl("Dogs, Cats & Mice") == "Dogs%2C%20Cats%20%26%20Mice")
+      check(encodeUrl("☃") == "%E2%98%83")
+
+  suite "test for hmacSha1":
+    # https://dev.twitter.com/oauth/overview/creating-signatures
+    test "test for hmacSha1 function.":
+      check(hmacSha1("kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE",
+                     "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521").toBase64 == "tnnArxj06cWHq44gCs1OSKk/jLY=")
