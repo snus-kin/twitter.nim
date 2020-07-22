40f2c1075855000a7dbeccd42f883092bb5137dd
snus-kin <tcarroll@snufk.in>
2020-07-22 23:16:54 +0100

    Add example of chunked media upload

--- examples/example.nim
+++ examples/example.nim
@@ -1,4 +1,4 @@
-import twitter, json, strtabs, httpclient
+import twitter, json, strtabs, httpclient, streams, os
 
 when isMainModule:
   var parsed = parseFile("credential.json")
@@ -14,14 +14,56 @@ when isMainModule:
   echo resp.status
 
   # ditto, but selected by screen name.
-  resp = twitterAPI.user("shitsyndrome")
+  resp = twitterAPI.user("sn_fk_n")
   echo resp.status
 
   # Using proc corresponding twitter REST APIs.
   resp = twitterAPI.userTimeline()
-  echo parseJson(resp.body)
+  echo pretty parseJson(resp.body)
 
   # Using `callAPI` template.
   var testStatus = {"status": "test"}.newStringTable
   resp = twitterAPI.callAPI(statusesUpdate, testStatus)
-  echo parseJson(resp.body)
+  echo pretty parseJson(resp.body)
+
+  # Upload files in a stream
+  const buffersize = 200000
+  let mediaStream = newFileStream("test.jpg", fmRead)
+  let mediaSize = "test.jpg".getFileSize
+
+  # INIT
+  resp = twitterAPI.mediaUploadInit("image/jpg", $ mediaSize)
+  let initResp = parseJson(resp.body)
+  let mediaId = initResp["media_id_string"].getStr
+
+  # APPEND
+  var buffer = ""
+  var segment = 0
+  var total = 0
+  if not isNil(mediaStream):
+    while not mediaStream.atEnd(): 
+      buffer = mediaStream.readStr(buffersize)
+      echo "Uploading segment: ", segment
+      resp = twitterAPI.mediaUploadAppend(mediaId, $ segment, buffer)
+      # Response should be 204
+      if resp.status != "204 No Content":
+        echo resp.status
+        echo pretty parseJson(resp.body)
+        stderr.writeLine "Error when uploading"
+        break
+      segment += 1
+      total += len(buffer)
+
+  # STATUS
+  resp = twitterAPI.mediaUploadStatus(mediaId)
+  # hopefully we don't have to poll it since we didn't upload async
+  echo pretty parseJson(resp.body)
+
+  # FINALIZE
+  resp = twitterAPI.mediaUploadFinalize(mediaId)
+  let final_media_id = parseJson(resp.body)["media_id_string"].getStr
+  echo final_media_id
+  
+  # Send a tweet with that media
+  var mediaStatus = {"status": "This is an image upload test", "media_ids": final_media_id}.newStringTable
+  resp = twitterAPI.statusesUpdate(mediaStatus)
